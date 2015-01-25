984fe8537dc4b913bc7ee0f24d33aa4f36190efc
devpassion <git@lacourarie.org>
2015-01-25 11:28:32 +0100

    Add RASP* variables in cmake files

--- cmake/pi.cmake
+++ cmake/pi.cmake
@@ -1,14 +1,11 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
-SET( RASPTOOLS_PATH /home/tim288/prog/lib/rasp )
 SET(CMAKE_C_COMPILER ${RASPTOOLS_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER ${RASPTOOLS_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++)
 SET(CMAKE_FIND_ROOT_PATH ${RASPTOOLS_PATH}/rootfs)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-
 
 
 
